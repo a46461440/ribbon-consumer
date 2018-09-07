c14dd3716c8db5215a4e1064ca86527067ffdb3f
ZXC <496461440@qq.com>
2018-09-08 00:03:45 +0800

    Initial commit

---
+++ src/main/java/com/springcloud/ribbonconsumer/action/ConsumerAction.java
@@ -0,0 +1,20 @@
+package com.springcloud.ribbonconsumer.action;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class ConsumerAction {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @RequestMapping(value = "/ribbon-consumer", method = RequestMethod.GET)
+    public String helloConsumer() {
+        return restTemplate.getForEntity("http://HELLO-PROVIDER/hello", String.class).getBody();
+    }
+
+}
